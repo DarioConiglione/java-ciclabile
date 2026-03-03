6d0eac253b1e86837297b796960e41616bba6fac
DarioConiglione <dariocng13@gmail.com>
2026-03-03 17:25:56 +0100

    put a method to add elements by creating a new array

--- src/org/lessons/java/lista/ListaInteri.java
+++ src/org/lessons/java/lista/ListaInteri.java
@@ -5,8 +5,14 @@ public class ListaInteri {
     private int[] elencoInteri;
     private int index;
 
+    public ListaInteri() {
+        this.elencoInteri = new int[0];
+        this.index = 0;
+    }
+
     public ListaInteri(int[] elencoInteri) {
         this.elencoInteri = elencoInteri;
+        this.index = 0;
     }
 
     public int getElementoSuccessivo() {
@@ -19,6 +25,13 @@ public class ListaInteri {
     public boolean hasAncoraElementi() {
         return index < elencoInteri.length;
 
+    }
+
+    public void addElemento() {
+        int[] nuovoElencoInteri = elencoInteri;
+        for (int i = 0; i < elencoInteri.length; i++) {
+            nuovoElencoInteri[i] = elencoInteri[i];
+        }
     }
 
 }

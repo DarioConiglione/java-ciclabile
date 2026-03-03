57ba14abf8e1ffb0b90c851f65ee19505972a927
DarioConiglione <dariocng13@gmail.com>
2026-03-03 16:45:15 +0100

    add methods

--- src/org/lessons/java/lista/ListaInteri.java
+++ src/org/lessons/java/lista/ListaInteri.java
@@ -2,4 +2,18 @@ package org.lessons.java.lista;
 
 public class ListaInteri {
 
+    private int[] elencoInteri;
+
+    public ListaInteri(int[] interi) {
+        this.elencoInteri = interi;
+    }
+
+    public void getElementoSuccessivo() {
+
+    }
+
+    public void hasAncoraElementi() {
+
+    }
+
 }
